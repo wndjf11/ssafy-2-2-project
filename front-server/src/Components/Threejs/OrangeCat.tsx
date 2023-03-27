d8801303fcd4c59c7576794f3ce89498a566bb34
KHminor <ghdals2020@gmail.com>
2023-03-28 01:08:53 +0900

    Feat: 추가 뱃지 & 도감 페이지, Threejs,, notice 페이지 수정

--- front-server/src/Components/Threejs/OrangeCat.tsx
+++ front-server/src/Components/Threejs/OrangeCat.tsx
@@ -17,15 +17,26 @@ function OrangeCat({sendEmo}:any) {
     }
   },[])
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full mt-2'>
       <Canvas style={{width: '100%', height: '100%', borderTopLeftRadius: '1rem', borderTopRightRadius: '1rem'}}>
         <mesh scale={1} position={[0,-2.4,2.5]}>
           <Suspense fallback={null}>
             <ambientLight />
-            <Sky azimuth={0.7}/>
-            <spotLight intensity={0.9} angle={0.1} penumbra={1} position={[10,15,10]} castShadow/>
+            <directionalLight 
+            castShadow
+            position={[0, 30, 0]}
+            intensity={1}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-far={50}
+            shadow-camera-left={-100}
+            shadow-camera-right={100}
+            shadow-camera-top={100}
+            shadow-camera-bottom={-100}
+        />
+            <Sky azimuth={0.8} sunPosition={[5, 30, 8]} distance={450000} inclination={0}/>
             {emo}
-            {/* <OrbitControls enablePan={true} enableZoom={false} enableRotate={true}/> */}
+            <OrbitControls enablePan={true} enableZoom={false} enableRotate={true}/>
           </Suspense>
         </mesh>
       </Canvas>
